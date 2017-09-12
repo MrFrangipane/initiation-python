b081dc970a8aa5bfa71f12c02da95d6d24ef3511
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2017-09-12 11:27:14 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,19 +1,15 @@
-from pprint import pprint
-
 bout_bois = {
   'name': 'Bout de bois tordu',
   'resistance': 1,
   'degat': 1,
   'poids': 1
 }
-
 saucisson = {
   'name': "Saucisson a l'ail bien sec",
   'resistance': 15,
   'degat': 3,
   'poids': 2
 }
-
 cornichons = {
   'name': "Cornichons marines",
   'resistance': 0,
@@ -23,3 +19,19 @@ cornichons = {
 
 mon_inventaire = [bout_bois, saucisson, cornichons]
 
+def calcul_statut(inventaire):
+  statut = {
+    'resistance': 0,
+    'degat': 0,
+    'poids': 0
+  }
+  
+  for item in inventaire:
+    statut['resistance'] += item['resistance']
+    statut['degat'] += item['degat']
+    statut['poids'] += item['poids']
+  
+  return statut
+
+statut = calcul_statut(inventaire=mon_inventaire)
+print statut
