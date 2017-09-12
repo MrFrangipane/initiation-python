7f90dfeb075c8d4239478a8ed12e87821d8df3ab
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2017-09-12 10:46:02 +0200

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+bout_bois = {
+  'name': 'Bout de bois tordu',
+  'resistance': 1,
+  'degat': 1,
+  'poids': 1
+}
+
+saucisson = {
+  'name': "Saucisson a l'ail bien sec",
+  'resistance': 15,
+  'degat': 3,
+  'poids': 2
+}
+
+cornichons = {
+  'name': "Cornichons marines",
+  'resistance': 0,
+  'degat': 1,
+  'poids': 1
+}
+
+mon_inventaire = [bout_bois, saucisson, cornichons]
+
