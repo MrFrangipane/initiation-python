abc5e3ce2d6e641ede54732f461a53451f4f74ad
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2017-09-14 11:00:31 +0200

    Update circles.py

--- circles.py
+++ circles.py
@@ -1,4 +1,5 @@
 import math
+import random
 import MaxPlus
 
 def compute_positions(radius, count):
@@ -23,6 +24,27 @@ def compute_rotations(count):
 	return rotations
 
 	
+def compute_colors(base_color, count):
+	colors = list()
+	alpha = (2 * math.pi) / count
+	
+	for i in range(count):
+		value = math.cos(i * alpha * 3)
+		color = (
+			base_color[0] * value,
+			base_color[1] * value,
+			base_color[2] * value,
+		)
+		colors.append(color)
+	
+	return colors
+
+
+def random_color():
+	return random.random(), random.random(), random.random()
+	
+	
+	
 def cylinder(height=1.0, radius=1.0):
 	obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Cylinder)
 	node = MaxPlus.Factory.CreateNode(obj)
@@ -36,30 +58,39 @@ def move(node, position):
 	node.Position = MaxPlus.Point3(*position)
 
 
+def set_color(node, color):
+	node.SetWireColor(MaxPlus.Color(*color))
+	
+
 def rotate(node, rotation):
 	rotation_quat = MaxPlus.Quat()
 	rotation_quat.SetEuler(*rotation)
 	node.Rotation = rotation_quat
 
 	
-def cylinder_circle(circle_radius, cylinder_count, cylinder_height, cylinder_radius):
+def cylinder_circle(circle_radius, cylinder_count, cylinder_height, cylinder_radius, base_color):
 	cylinders = list()
 	cylinder_positions = compute_positions(circle_radius, cylinder_count)
 	cylinder_rotations = compute_rotations(cylinder_count)
+	cylinder_colors = compute_colors(base_color, cylinder_count)
 	
 	for index, cylinder_position in enumerate(cylinder_positions):
 		new_cylinder = cylinder(cylinder_height, cylinder_radius)
+		
 		move(new_cylinder, cylinder_position)
 		rotate(new_cylinder, cylinder_rotations[index])
+		set_color(new_cylinder, cylinder_colors[index])
+		
 		cylinders.append(new_cylinder)
 
 	return cylinders
 
 
-for i in range(1, 6):
+for i in range(1, 20):
 	cylinders = cylinder_circle(
-		circle_radius=i * 5.0, 
-		cylinder_count=i * 7,
-		cylinder_height=i * 3,
-		cylinder_radius=i * 0.5
+		circle_radius=i * 7, 
+		cylinder_count=i * 5,
+		cylinder_height=i * 1.1,
+		cylinder_radius=2,
+		base_color=random_color()
 )
