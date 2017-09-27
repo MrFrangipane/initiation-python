1164529ae45791cdc479e2be81c3c141bd4c8a9e
MrFrangipane <mrfrangipane@frangipane.com>
2017-09-27 09:11:05 +0200

    Correction mise en forme, conventions de nommage etc

--- corrections/andre-yoann.py
+++ corrections/andre-yoann.py
@@ -1,102 +1,124 @@
-#actionMan.executeAction 0 "40021";delete $; python.ExecuteFile @"F:\_DD_WORK\IIM\exo_python\pythonmax_ExoSquare.py"
-#_______09.2017
-#_____Yoann André IIM A5 3D 
-#______ EXO PYTHON - 3DSMAX 2016 
-import math
-import MaxPlus
+# actionMan.executeAction 0 "40021";delete $; python.ExecuteFile @"F:\_DD_WORK\IIM\exo_python\pythonmax_ExoSquare.py"
+# 09.2017
+# Yoann Andre IIM A5 3D
+# EXO PYTHON - 3DSMAX 2016
+
+
 import random
-#------------- 3DS MAX -----------
-def cylinder(radius,height): #création d'un cylinder
-	obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Cylinder)
-	node = MaxPlus.Factory.CreateNode(obj)
-	obj.ParameterBlock.Radius.Value = radius
-	obj.ParameterBlock.Height.Value = height
-	return node
+import MaxPlus
+
+
+#
+## 3DS MAX
+def cylinder(radius,height):
+    """creation d'un cylinder"""
+    obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Cylinder)
+    node = MaxPlus.Factory.CreateNode(obj)
+    obj.ParameterBlock.Radius.Value = radius
+    obj.ParameterBlock.Height.Value = height
+    return node
+
 
 def colorize(node, color):
-	node.SetWireColor(MaxPlus.Color(*color))
-	
-def move(node, position): #Positionné le cylinder
-	node.Position = MaxPlus.Point3(*position)
+    node.SetWireColor(MaxPlus.Color(*color))
+
+
+def move(node, position):
+    """Positionner le cylinder"""
+    node.Position = MaxPlus.Point3(*position)
+
+
 def set_height(node, height):
-	obj = node.GetBaseObject()
-	obj.ParameterBlock.Height.SetValue(height)
-	
-#------------- CALCULS -----------
+    obj = node.GetBaseObject()
+    obj.ParameterBlock.Height.SetValue(height)
+
+
+#
+## CALCULS
 def compute_random_heights(count,height_min, height_max): 
-	random_heights = list()
+    random_heights = list()
 	
-	for i in range(0,count):
-	  nb = random.randint(height_min,height_max)
-	  random_heights.append(nb)
+    for i in range(0,count):
+        random_height = random.randint(height_min,height_max)
+        random_heights.append(random_height)
 
-	return random_heights #renvoie d'une liste avec des valeurs random
-	
-def padding(count,radius): #pas entre les cylinders
-	padding_list = list()
-	if radius <= 1:
-	  radius += 1
-	for i in range(0,count):
-		padding_list.append(i*(radius*3))
+    return random_heights   # renvoi d'une liste avec des valeurs random
 
-	return padding_list #revoie du "pas" entre les objets
+
+def padding(count, radius):
+    """pas entre les cylinders"""
+    padding_list = list()
+
+    if radius <= 1: radius += 1
+
+    for i in range(0,count):
+        padding_list.append(i * radius * 3)
+
+    return padding_list  # renvoi du "pas" entre les objets
 	
 
 def compute_position(count,radius):
-	position = list()
-	for x in radius:
-		for y in radius:
-			position.append([x,y,0])
+    positions = list()
+    for x in radius:
+        for y in radius:
+            positions.append([x,y,0])
 	
-	return position #renvoie d'une liste de position
+    return positions  # renvoi d'une liste de position
+
 
 def compute_color(base_color1, base_color2, count):
-	color = list()
-	for x in range(0,count):
-		for y in range(0,count):
-			if y%2: #si [y] est divisible par 2 alors mets la color 1
-				color.append(base_color1)
-			else: # sinon mets la 2
-				color.append(base_color2)
+    color = list()
+    for x in range(0,count):
+        for y in range(0,count):
+            if y % 2:  # si [y] est divisible par 2 alors mets la color 1
+                color.append(base_color1)
+            else:  # sinon mets la 2
+                color.append(base_color2)
 
-	return color #liste avec une ligne sur 2 base_color1 et base_color2
+    return color  # liste avec une ligne sur 2 base_color1 et base_color2
 
 
-#------------- CHEF -----------
-def cylinder_square (cylinder_radius, cylinder_height, square_count, base_color1, base_color2): # CHEF 
-	cylinders = list()
-	radius_list = padding(square_count,cylinder_radius)
-	cylinder_positions = compute_position(square_count,radius_list)
-	cylinder_color = compute_color(base_color1, base_color2, square_count)
+#
+## CHEF
+def cylinder_square (cylinder_radius, cylinder_height, square_count, base_color1, base_color2):
+    """Chef"""
+    cylinders = list()
+    radiuses = padding(square_count, cylinder_radius)
+    cylinder_positions = compute_position(square_count, radiuses)
+    cylinder_color = compute_color(base_color1, base_color2, square_count)
 	
-	for index in range(square_count*square_count):
-		new_cylinder = cylinder(cylinder_radius, cylinder_height)
+    for index in range(square_count * square_count):
+        new_cylinder = cylinder(cylinder_radius, cylinder_height)
 
-		move(new_cylinder, cylinder_positions[index])
-		colorize(new_cylinder, cylinder_color[index])
-		cylinders.append(new_cylinder)
+        move(new_cylinder, cylinder_positions[index])
+        colorize(new_cylinder, cylinder_color[index])
+        cylinders.append(new_cylinder)
 
-	return cylinders
-#------------- CHEF ANIMATION -----------
+    return cylinders
+
+#
+## CHEF ANIMATION
 def animate_cylinders(cylinders, frame_count, height_min, height_max):
-	heights = compute_random_heights(frame_count, height_min, height_max)
+    heights = compute_random_heights(frame_count, height_min, height_max)
 	
-	MaxPlus.Animation.SetAnimateButtonState(True)
+    MaxPlus.Animation.SetAnimateButtonState(True)
 	
-	for frame_number in range(frame_count):
-		MaxPlus.Animation.SetTime(frame_number * 160)
+    for frame_number in range(frame_count):
+        MaxPlus.Animation.SetTime(frame_number * 160)
 		
-		for index, cylinder in enumerate(cylinders):
-			i = (frame_number + index) % frame_count
-			set_height(cylinder, heights[i])
+        for index, cylinder in enumerate(cylinders):
+            i = (frame_number + index) % frame_count
+            set_height(cylinder, heights[i])
 			
-	MaxPlus.Animation.SetAnimateButtonState(False)
+    MaxPlus.Animation.SetAnimateButtonState(False)
 
-#------------- Definition -----------
+
+#
+# Main
 if __name__ == '__main__':
-	all_objects = list()
+    all_objects = list()
 	
-	cylinder = cylinder_square(
+    cylinders = cylinder_square(
 		cylinder_radius = 3.0,
 		cylinder_height = 5.0,
 		square_count = 5,
@@ -104,8 +126,6 @@ if __name__ == '__main__':
 		base_color2 = [1,0,0.5]
 
 	)
-	all_objects += cylinder
+    all_objects += cylinders
 	
-animate_cylinders(all_objects, frame_count = 60, height_min = 1, height_max = 10) 
-	
-	
+    animate_cylinders(all_objects, frame_count=60, height_min=1, height_max=10)
--- corrections/laruelle-alexandre.py
+++ corrections/laruelle-alexandre.py
@@ -1,81 +1,146 @@
 import math
-import random
 import MaxPlus
 
-def createCube(length, width, height, position):					#fonction créer un cube.
-	obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Box) 	#la variable obj contient les infos de l'objet géometrique, ici une Box.
-	node = MaxPlus.Factory.CreateNode(obj)							#on créer la variable Node qui est notre noeud lié au viewport et le lier a notre obj.
-	obj.ParameterBlock.length.Value = length						#on définit la longueur de l'objet.
-	obj.ParameterBlock.width.Value = width							#on définit la largeur de l'objet.
-	obj.ParameterBlock.Height.Value = height						#on définit la hauteur de l'objet.
-	node.Position = MaxPlus.Point3(*position)						#on place notre box dans la scene grace a sa position. *position représente un ensemble de données x, y, z qui sont les coordonnées de l'objet dans la scene.
-	return node														#la fonction renvoi notre node contenant la box créer au dessus.
-	
-def compute_positions(espacement, width, length):	#fonction de calcule des coordonnées de position de nos futures objets.
-	positions = list()								#on créer une liste vide qui contiendra nos positions.
-	for ligneY in range(width):						#pour chaques lignes en Y en fonction du nombre width.
-		for ligneX in range(length):				#on créer une ligne en X en fonction du nombre length.
-			x = ligneX*espacement					#la variable x contient la position en x fois un espacement pour pas que les cubes soient les uns sur les autres.
-			y = ligneY*espacement					#la variable y contient la position en y fois un espacement pour pas que les cubes soient les uns sur les autres.
-			positions.append([x, y, 0.0])			#et on range ces infos dans notre liste de coordonnées x y z. A noter que Z est a 0 tout le temps car on veut que toutes les boites soit au sol; à 0 en Z donc.
-	return positions								#la fonction renvoi notre liste contenant les positions calculées.
 
-def set_height(node, height):					#fonction pour définir/modifier la hauteur de notre box.
-	obj = node.GetBaseObject()					#obj récupere les parametres de la box via node.
-	obj.ParameterBlock.Height.SetValue(height)	#initialisation de la nouvelle valeur de hauteur de notre cube.
-	
-def compute_heights(period,countCarres,sizeMultiplier):	#fonction de calcule des hauteurs pour l'animation.
-	alpha = (2*period*math.pi) / countCarres			#calcule de alpha qui correspond au nombre de fois que l'on va créer un point sur notre cerlce. pour ce faire, 2pi est multiplié par la periode (rayon du cerle où les points sont calculés) le tout divisé par le nombre de carrés que l'ont a créé.
-	heights = list()									#on créer une liste vide qui va acceuille nos données de hauteurs.
-	for i in range(0,countCarres):						#boucle qui, pour le nombre de carrés (countCarres), va calculer la hauteur de chaques point dans le cercle.
-		height = (math.sin(alpha * i))*sizeMultiplier	#pour obtenir la hauteur du point, il faut calculer sin(Angle * le num du carré). je multiplis cette valeur par sizeMultiplier pour agrandir les hauteurs car trop petite dans la scene plus de visibilité).
-		heights.append(height)							#on ajoute la hauteur dans la liste, les un apres les autres pendant la boucle.
-	return heights										#on renvoi ces infos à celui qui appelera cette fonction.
+def createCube(length, width, height, position):
+    """
+    fonction creer un cube.
+    la variable obj contient les infos de l'objet geometrique, ici une Box.
+    on creer la variable Node qui est notre noeud lie au viewport et le lier a notre obj.
+    on definit la longueur de l'objet.
+    on definit la largeur de l'objet.
+    on definit la hauteur de l'objet.
+    on place notre box dans la scene grace a sa position.
+    *position represente un ensemble de donnees x, y, z qui sont les coordonnees de l'objet dans la scene.
+    la fonction renvoi notre node contenant la box creee au dessus.
+    """
+    obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Box)
+    obj.ParameterBlock.length.Value = length
+    obj.ParameterBlock.width.Value = width
+    obj.ParameterBlock.Height.Value = height
 
-	
-def animate_Boxes(Boxes, period, count_carres,frame_count,size_multiplier):	#Fonction pour animer les boites.
-	heights = compute_heights(period, count_carres,size_multiplier)			#on récupere les hauteurs calculées dans une liste "heights".
-	MaxPlus.Animation.SetAnimateButtonState(True)							#on active "autokey".
-	for frame_number in range(frame_count):									#pour chaque frames.
-		MaxPlus.Animation.SetTime(frame_number * 160)						#truc chelou de max car pour lui, il y a 160 trucs dans une frame. On doit le mettre sans se poser de questions.
-		for index, Box in enumerate(Boxes):									#pour chaques box crées.
-			i = (frame_number + index) % count_carres						#i nous permet d'incrémenter de 1 notre lecture de la liste de hauteurs pour que les boites bouches (sinon elles sont animé mais la hauteur ne change pas donc inutile
-			set_height(Box, heights[i])										#on défini donc notre nouvelle hauteur à notre box.
-			Color_Ratio = heights[i] / 10									#je divise les hauteurs par 10 pour avoir plus de valeurs entre 0 et 1 plutot que entre 0 et 10 car les valeurs de couleurs vont de 0 à 1.
-			set_color(Box,Color_Ratio,0,0)									#j'initialise la nouvelle couleur. ici avec des tons de rouge vers le noir
-			
-	MaxPlus.Animation.SetAnimateButtonState(False)							#on désactive "autokey"
+    node = MaxPlus.Factory.CreateNode(obj)
+    node.Position = MaxPlus.Point3(*position)
 
-def set_color(node, R, G, B):					#fonction pour changer la couleur de l'objet
-	node.SetWireColor(MaxPlus.Color(R,G,B))		#changement de la couleur en spécifiant notre node
-	
-
-period = 1				#défini le nombre de fois que l'on va fair le tour du cerle dans le calcule des positions
-nb_Frames = 50			#nombre de frames que l'on veut calculer l'animation
-size_multiplier=20		#pour changer de coefficient de hauteur (optionnel mais 10 est une bonne valeur) 
-padding = 15			#espacement entre nos cubes (doit toujours etre supérieur à la largeur du cube à savoir 10)
-nb_Box_width=20			#largeur de l'objet
-nb_Box_length=20		#longeur de l'objet
+    return node
 
 
-list_position = compute_positions(padding, nb_Box_width, nb_Box_length)	#calcule des positions des objets
-all_Boxes=list()														#création d'une liste vide pour les boites
+def compute_positions(spacing, width, length):
+    """
+    fonction de calcule des coordonnees de position de nos futures objets.
+    on creer une liste vide qui contiendra nos positions.
+    pour chaques lignes en Y en fonction du nombre width.
+    on creer une ligne en X en fonction du nombre length.
+    la variable x contient la position en x fois un espacement pour pas que les cubes soient les uns sur les autres.
+    la variable y contient la position en y fois un espacement pour pas que les cubes soient les uns sur les autres.
+    et on range ces infos dans notre liste de coordonnees x y z.
+    A noter que Z est a 0 tout le temps car on veut que toutes les boites soit au sol; a 0 en Z donc.
+    la fonction renvoi notre liste contenant les positions calculees.
+    """
+    positions = list()
 
-for position in list_position:											#création des cubes en fonction des positions calculées précédement
-	
-	Boxes=createCube(10,10,10,position)									#on créer le cube à tel position
-	set_color(Boxes,1,1,1)												#on initialise la couleur en blanc
-	all_Boxes.append(Boxes)												#on remplit la liste des boites
-	
+    for ligne_y in range(width):
+        for ligne_x in range(length):
+            x = ligne_x * spacing
+            y = ligne_y * spacing
+            positions.append([x, y, 0.0])
 
-animate_Boxes(all_Boxes,period,nb_Box_length,nb_Frames,size_multiplier)	#on lance le calcule de l'animation des boites
-	
-
-	
-	
-	
-
-	
+    return positions
 
 
+def set_height(node, height):
+    """
+    fonction pour definir/modifier la hauteur de notre box.
+    obj recupere les parametres de la box via node.
+    initialisation de la nouvelle valeur de hauteur de notre cube.
+    """
+    obj = node.GetBaseObject()
+    obj.ParameterBlock.Height.SetValue(height)
+
+
+def compute_heights(period, square_count, size_multiplier):
+    """
+    fonction de calcule des hauteurs pour l'animation.
+    calcule de alpha qui correspond au nombre de fois que l'on va creer un point sur notre cerlce.
+    pour ce faire, 2pi est multiplie par la periode (rayon du cerle ou les points sont calcules)
+    le tout divise par le nombre de carres que l'ont a cree.
+    on creer une liste vide qui va acceuille nos donnees de hauteurs.
+    boucle qui, pour le nombre de carres (square_count), va calculer la hauteur de chaques point dans le cercle.
+    pour obtenir la hauteur du point, il faut calculer sin(Angle * le num du carre).
+    je multiplis cette valeur par sizeMultiplier pour agrandir
+    les hauteurs car trop petite dans la scene plus de visibilite).
+    on ajoute la hauteur dans la liste, les un apres les autres pendant la boucle.
+    on renvoi ces infos a celui qui appelera cette fonction.
+    """
+    alpha = (2 * period * math.pi) / square_count
+    heights = list()
+
+    for i in range(0, square_count):
+        height = (math.sin(alpha * i)) * size_multiplier
+        heights.append(height)
+
+    return heights
+
 	
+def animate_Boxes(boxes, period, square_count, frame_count, size_multiplier):
+    """
+    Fonction pour animer les boites.
+    on recupere les hauteurs calculees dans une liste "heights".
+    on active "autokey".
+    pour chaque frames.
+    truc chelou de max car pour lui, il y a 160 trucs ("ticks") dans une frame.
+    On doit le mettre sans se poser de questions.
+    pour chaques box crees.
+    i nous permet d'incrementer de 1 notre lecture de la liste de hauteurs pour que les boites bouches
+    (sinon elles sont animees mais la hauteur ne change pas donc inutile
+    on defini donc notre nouvelle hauteur a notre box.
+    je divise les hauteurs par 10 pour avoir plus de valeurs entre 0 et 1 plutot que entre 0 et 10
+    car les valeurs de couleurs vont de 0 a 1.
+    j'initialise la nouvelle couleur. ici avec des tons de rouge vers le noir
+    """
+    heights = compute_heights(period, square_count, size_multiplier)
+
+    MaxPlus.Animation.SetAnimateButtonState(True)
+
+    for frame_number in range(frame_count):
+
+        MaxPlus.Animation.SetTime(frame_number * 160)
+
+        for index, box in enumerate(boxes):
+            i = (frame_number + index) % square_count
+            set_height(box, heights[i])
+            color_ratio = heights[i] / 10
+            set_color(box,color_ratio,0,0)
+
+    # on desactive "autokey"
+    MaxPlus.Animation.SetAnimateButtonState(False)
+
+
+def set_color(node, r, g, b):
+    """
+    fonction pour changer la couleur de l'objet
+    changement de la couleur en specifiant notre node
+    """
+    node.SetWireColor(MaxPlus.Color(r, g, b))
+	
+
+if __name__ == '__main__':
+    PERIOD = 1              # defini le nombre de fois que l'on va fair le tour du cerle dans le calcule des positions
+    FRAME_COUNT = 50        # nombre de frames que l'on veut calculer l'animation
+    SIZE_MULTIPLIER = 20    # pour changer de coefficient de hauteur (optionnel mais 10 est une bonne valeur)
+    PADDING = 15            # espacement entre nos cubes (doit toujours etre superieur a la largeur du cube a savoir 10)
+    BOX_WIDTH_COUNT = 20    # largeur de l'objet
+    BOX_HEIGHT_COUNT = 20   # longeur de l'objet
+
+
+    positions = compute_positions(PADDING, BOX_WIDTH_COUNT, BOX_HEIGHT_COUNT)  # calcule des positions des objets
+    all_Boxes = list()  # creation d'une liste vide pour les boites
+
+    for position in positions:  # creation des cubes en fonction des positions calculees precedement
+
+        boxes = createCube(10, 10, 10, position)  # on creer le cube a tel position
+        set_color(boxes, 1, 1, 1)  # on initialise la couleur en blanc
+        all_Boxes.append(boxes)  # on remplit la liste des boites
+
+    # on lance le calcule de l'animation des boites
+    animate_Boxes(all_Boxes, PERIOD, BOX_HEIGHT_COUNT, FRAME_COUNT, SIZE_MULTIPLIER)
