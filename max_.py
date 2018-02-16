61d17e12b0b920edc176af81989d2eebdd838e5c
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2018-02-16 11:34:05 +0100

    Update max_.py

--- max_.py
+++ max_.py
@@ -1,5 +1,16 @@
+import ctypes
+from PySide import QtCore, QtGui
 import MaxPlus
 
+
+GWL_HWNDPARENT = -8
+SetWindowLongPtr = ctypes.windll.user32.SetWindowLongPtrW
+
+
+class FocusFilter(QtCore.QObject):
+    def eventFilter(self, obj, event):
+        MaxPlus.CUI.DisableAccelerators()
+	return False
 
 def cylinder(height, radius, position, segments=1):
 	base_obj = MaxPlus.Factory.CreateGeomObject(MaxPlus.ClassIds.Cylinder)
@@ -31,3 +42,17 @@ def bend(node, angle, collapse=False):
 		return node
 	
 	return modifier
+
+def get_hwnd(widget):
+	ctypes.pythonapi.PyCObject_AsVoidPtr.restype = ctypes.c_void_p
+	ctypes.pythonapi.PyCObject_AsVoidPtr.argtypes = [ctypes.py_object]
+	wdgt_ptr = ctypes.pythonapi.PyCObject_AsVoidPtr(widget.winId())
+	return wdgt_ptr
+	
+def attach_widget(widget):
+	parent_hwnd = MaxPlus.Win32.GetMAXHWnd()
+	hwnd = get_hwnd(widget)
+	SetWindowLongPtr(hwnd, GWL_HWNDPARENT, parent_hwnd)
+	app = QtGui.QApplication.instance()
+	widget._focus_filter = FocusFilter()
+	widget.event_filter = app.installEventFilter(widget._focus_filter)
