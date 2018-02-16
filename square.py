8bc5bdb52b4dc5ad373474a7ac614157866bd027
Mr Frangipane <MrFrangipane@users.noreply.github.com>
2018-02-16 10:00:35 +0100

    square.py

---
+++ square.py
@@ -0,0 +1,41 @@
+import random
+import max_
+reload(max_)
+
+
+def make_cylinders(row_count, column_count, spacing, cyl_height, cyl_radius):
+	cylinders = []
+
+	for row in range(row_count):
+		for column in range(column_count):
+			
+			position = [row * spacing, column * spacing, 0]
+			new_cylinder = max_.cylinder(
+				cyl_height, 
+				cyl_radius, 
+				position, 
+				segments=5
+			)
+			
+			bend_angle = random.randint(-90, 90)
+			max_.bend(
+				new_cylinder, 
+				bend_angle, 
+				collapse=True
+			)
+			
+			rotation = [0, 0, random.randint(0, 360)]
+			max_.rotate(new_cylinder, rotation)
+			
+			cylinders.append(new_cylinder)
+	
+	return cylinders
+
+
+make_cylinders(
+	row_count=15, 
+	column_count=5, 
+	spacing=15, 
+	cyl_height=10, 
+	cyl_radius=5
+)
